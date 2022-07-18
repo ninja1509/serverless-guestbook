7911ad73ef1b8ebcc6337515baafc290eb2052b1
ninja1509 <63071772+ninja1509@users.noreply.github.com>
2022-07-18 09:08:05 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://c2d4685c.eu-gb.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://9e81f021.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
